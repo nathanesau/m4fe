ee189e517c603dfb2f7e2ebc1a5b009e5cbdc02a
Nathan Esau <nathanesau1@gmail.com>
2014-09-16 02:07:44 +0000

    server commit

--- R/blackScholes.R
+++ R/blackScholes.R
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-#' @title Black-Scholes Formula (European Option)
-#' @description The famous Black-Scholes Option Pricing Formula based on the Lognormal Models. This formula can be eKtended to barrier options, currency options, options on futures, etc.
-#' @param S The Stock Price
-#' @param K The Strike Price
-#' @param r The risk-free continuously compounded interest rate
-#' @param T The expiration date
-#' @param sigma The volatility
-#' @details The Black-Scholes Formula is based on the assumption of geometric brownian motion and can be shown to satisfy the Black-Scholes Partial Differential Equation. It can be thought of as the combination of an asset-or-nothing option and a cash-or-nothing option
-#' @examples blackScholes(S=41,K=40,r=0.08,T=1,sigma=0.3)
-#' @export
-blackScholes <- function(S, K, r, T, sigma) {  
-  d1 <- (log(S/K)+(r+sigma^2/2)*T)/sigma*sqrt(T)
-  d2 <- d1 - sigma * sqrt(T)
-  
-  call = S*pnorm(d1) - K*exp(-r*T)*pnorm(d2)
-  put  =  K*exp(-r*T) * pnorm(-d2) - S*pnorm(-d1)
-  return(call)
-=======
 #' @title Black-Scholes Formula (European Option)
 #' @description The famous Black-Scholes Option Pricing Formula based on the Lognormal Models. This formula can be extended to barrier options, currency options, options on futures, etc.
 #' @param S The Stock Price
@@ -34,5 +15,4 @@ blackScholes <- function(S, K, r, T, sigma) {
   call = S*pnorm(d1) - K*exp(-r*T)*pnorm(d2)
   put  =  K*exp(-r*T) * pnorm(-d2) - S*pnorm(-d1)
   return(call)
->>>>>>> 7a83811598d915ec14910c06b8abc747a8494f97
 }
--- R/lognormal.R
+++ R/lognormal.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 lognormal.params = list(mu=2, sigma=1.9555)
 
 dlognormal <- function(x, mu=lognormal.params$mu, sigma=lognormal.params$sigma) {
@@ -12,19 +11,4 @@ Elognormal <- function(mu=lognormal.params$mu, sigma=lognormal.params$sigma) {
 
 E2lognormal <- function(mu=lognormal.params$mu, sigma=lognormal.params$sigma) {
   exp(2*mu + 2*sigma^2)
-=======
-lognormal.params = list(mu=2, sigma=1.9555)
-
-dlognormal <- function(x, mu=lognormal.params$mu, sigma=lognormal.params$sigma) {
-  z=(log(x)-mu)/sigma
-  exp(-z^2/2)/(x*sigma*sqrt(2*pi))
-}
-
-Elognormal <- function(mu=lognormal.params$mu, sigma=lognormal.params$sigma) {
-  exp(mu+0.5*sigma^2)
-}
-
-E2lognormal <- function(mu=lognormal.params$mu, sigma=lognormal.params$sigma) {
-  exp(2*mu + 2*sigma^2)
->>>>>>> 7a83811598d915ec14910c06b8abc747a8494f97
 }
